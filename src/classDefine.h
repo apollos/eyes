90b48672e14240749bd52eb25dc7db56b943c399
Song Yu <apollos521@gmail.com>
2016-07-28 20:08:51 +0800

    update makefile

--- src/classDefine.h
+++ src/classDefine.h
@@ -1,11 +1,7 @@
 #ifndef CLASSDEFINE_H_
 #define CLASSDEFINE_H_
 
-/*
 #define CLASS_LABLE_SIZE 20
 static char *class_names[] = {"aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"};
-*/
 
-#define CLASS_LABLE_SIZE 3
-static char *class_names[] = {"stop sign", "yield sign", "person"};
 #endif
