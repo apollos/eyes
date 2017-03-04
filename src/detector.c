e9ac4ba1d645ccf466764bd67f02e6dfcb9510b5
Song Yu <apollos521@gmail.com>
2017-03-04 17:41:19 +0800

    adjust the neural network size for high resolution

--- src/detector.c
+++ src/detector.c
@@ -80,9 +80,10 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     while(get_current_batch(net) < net.max_batches){
         if(l.random && count++%10 == 0){
             printf("Resizing\n");
-            int dim = (rand() % 10 + 10) * 32;
-            if (get_current_batch(net)+200 > net.max_batches) dim = 608;
+            //int dim = (rand() % 10 + 10) * 32;
+            //if (get_current_batch(net)+200 > net.max_batches) dim = 608;
             //int dim = (rand() % 4 + 16) * 32;
+            int dim = (args.w <= args.h ? args.w : args.h);
             printf("%d\n", dim);
             args.w = dim;
             args.h = dim;
