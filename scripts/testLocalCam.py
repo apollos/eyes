2b75752c09e82f8e649f28aa3a163eab09e95a42
Yu Song <apollos521@hotmail.com>
2018-01-26 15:19:52 +0800

    test write mp4

--- scripts/testLocalCam.py
+++ scripts/testLocalCam.py
@@ -1,7 +1,7 @@
 import sys
 import cv2  
 import numpy
-import cv2.cv as cv
+#import cv2.cv as cv
 import socket
 import os
 import struct
@@ -17,8 +17,8 @@ if __name__ == "__main__":
         print("Can not open Cam!")
         exit(-1)
     #cv2.namedWindow("camera_Capture", cv.CV_WINDOW_AUTOSIZE)  
-    capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, 640*0.8)  
-    capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 480*0.8)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640*0.8)  
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480*0.8)
     #get image size
     ret, img = capture.read()
     high, width, channel = img.shape
@@ -26,18 +26,22 @@ if __name__ == "__main__":
     
     encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),80]
     dim = (640, 480)
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (640,480))
     while True:  
         ret, img = capture.read()
         
         if(not ret):
-            capture.set(cv.CV_CAP_PROP_POS_FRAMES,int(count/percentagePos));
+            capture.set(cv2.CAP_PROP_POS_FRAMES,int(count/percentagePos));
             continue
         
         img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
         
         #time.sleep(0.1)  
+        #frame = cv2.flip(img,0)
+        out.write(img)
         cv2.imshow("camera_Capture", img)  
-        if cv2.waitKey(50) == 122:  
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break  
           
     cv2.destroyAllWindows() 
